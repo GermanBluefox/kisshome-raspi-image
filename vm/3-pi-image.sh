c3d1678d33461fb17ca7897427b31d66c822bdfc
Bluefox <dogafox@gmail.com>
2024-09-16 11:26:25 +0000

    All files prepered

--- vm/3-pi-image.sh
+++ vm/3-pi-image.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
+sudo apt-get install qemu-utils
 sudo docker pull ptrsr/pi-ci
+
+sudo mkdir /dist
+sudo cp ./files/* /dist
+
 # Resize image to 4GB
-sudo docker run --rm -it ptrsr/pi-ci resize 4G
-sudo docker run --rm -it -v $PWD/dist:/dist ptrsr/pi-ci start
+sudo docker run --rm -it -v /dist:/dist ptrsr/pi-ci resize 4G
+
+sudo docker run --rm -it -v /dist:/dist ptrsr/pi-ci sudo /usr/bin/bash /dist/install.sh
+
--- vm/files/install.sh
+++ vm/files/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 sudo systemctl enable ssh
 sudo systemctl start ssh
+
 sudo useradd -m -G root iob
 # create iobroker user
 echo "iob:2024=smart!" | sudo chpasswd
@@ -8,6 +9,17 @@ echo "iob:2024=smart!" | sudo chpasswd
 echo "root:2024=smartroot!" | sudo chpasswd
 sudo sed -i 's/^#\?\s*PermitRootLogin\s.*/PermitRootLogin no/' "/etc/ssh/sshd_config"
 sudo systemctl restart sshd
+
+# Copy rc.local to iob user
+sudo cp /dist/rc.local /etc/rc.local
+sudo chwon iob:root /etc/rc.local
+sudo chmod 775 /etc/rc.local
+sudo systemctl enable rc-local
+
+sudo cp /dist/firstboot.sh /home/iob/firstboot.sh
+sudo chwon iob:root /home/iob/firstboot.sh
+sudo chmod 775 /home/iob/firstboot.sh
+
 
 echo =============================================================
 echo Setup timezone
@@ -21,4 +33,5 @@ echo =============================================================
 sudo apt-get -y install net-tools curl wget openssh-server software-properties-common build-essential ca-certificates nano
 curl -sLf https://iobroker.net/install.sh | bash -
 systemctl stop iob
-iob unsetup
+iob unsetup -y
+# now you can stop the docker and create an image
