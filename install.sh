45027d7c08beace6e97e90f7c63f0da5169429f3
GermanBluefox <dogafox@gmail.com>
2024-09-16 22:45:51 +0800

    Update script

--- install.sh
+++ install.sh
@@ -3,26 +3,14 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo systemctl enable ssh
 sudo systemctl start ssh
-sudo apt-get install -y ntpdate
 
-sudo useradd -m -G root iob
 # create iobroker user
+sudo useradd -m -G root iob
 echo "iob:2024=smart!" | sudo chpasswd
 # set root password
 echo "root:2024=smartroot!" | sudo chpasswd
 sudo sed -i 's/^#\?\s*PermitRootLogin\s.*/PermitRootLogin no/' "/etc/ssh/sshd_config"
 sudo systemctl restart sshd
-
-# Copy rc.local to iob user
-sudo cp /opt/kisshome-raspi-image/vm/files/rc.local /etc/rc.local
-sudo chown iob:root /etc/rc.local
-sudo chmod 775 /etc/rc.local
-sudo systemctl enable rc-local
-
-sudo cp /opt/kisshome-raspi-image/vm/files/firstboot.sh /home/iob/firstboot.sh
-sudo chown iob:root /home/iob/firstboot.sh
-sudo chmod 775 /home/iob/firstboot.sh
-
 
 echo =============================================================
 echo Setup timezone
@@ -36,9 +24,15 @@ echo =============================================================
 sudo apt-get -y install net-tools curl wget openssh-server software-properties-common build-essential ca-certificates nano
 curl -sLf https://iobroker.net/install.sh | bash -
 cd /opt/iobroker
+sudo iob stop
+# Settings for kisshome
+sudo npm install iobroker.admin@latest --unsafe-perm
 sudo npm install iobroker.kisshome-research
-iob fix
-iob add kisshome-research
-systemctl stop iob
-iob unsetup -y
-# now you can stop the docker and create an image
+sudo iob fix
+sudo iob add kisshome-research
+sudo iob u admin
+# End of settings for kisshome
+sudo iob unsetup -y
+
+rm -rf /opt/kisshome-raspi-image/
+# now you can stop the raspberrypi and create an image
--- vm/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-echo sudo apt update
-sudo apt install -y git
-cd /opt
-sudo git clone https://github.com/GermanBluefox/kisshome-raspi-image
-sudo chmod +x /opt/kisshome-raspi-image/vm/files/install.sh
-sudo /opt/kisshome-raspi-image/vm/files/install.sh
