d8a72b12d083b7df16e58cf16f481e8ab4af03cd
Bluefox <dogafox@gmail.com>
2024-09-16 19:56:11 +0800

    prepare

--- vm/3-pi-image.sh
+++ vm/3-pi-image.sh
@@ -6,7 +6,9 @@ sudo mkdir -p /dist
 sudo cp ./files/* /dist
 
 # Resize image to 4GB
-sudo docker run --rm -it -v /dist:/dist ptrsr/pi-ci resize 4G
+sudo docker run --rm -it -v /dist:/dist ptrsr/pi-ci resize -y 4G
 
-sudo docker run --rm -it -v /dist:/dist ptrsr/pi-ci sudo /usr/bin/bash /dist/install.sh
+echo Run command in emulator sudo chmod +x /dist/install.sh && /usr/bin/bash /dist/install.sh
+sudo docker run --rm -p 22:22 -v /dist:/dist ptrsr/pi-ci start
+# sudo docker run --rm -it -v /dist:/dist ptrsr/pi-ci /usr/bin/bash /dist/install.sh
 
