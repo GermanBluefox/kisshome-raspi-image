75a70b936e34ed97cf528c18660bfb8b69bc28b1
GermanBluefox <dogafox@gmail.com>
2024-10-03 19:53:44 +0800

    Update instructions

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 Creates Raspberry Pi Image with preinstalled kisshome-research adapter
 
 ## How to start
-Create first with [Raspberry Pi Imager](https://www.raspberrypi.com/software/) the raspi image.
+Create first with [Raspberry Pi Imager](https://www.raspberrypi.com/software/) the Raspberry Pi 5 image.
 
 ![step1](step1.png)
 
 ![step2](step2.png)
 
 ```bash
+sudo sed -i 's/^#NTP=.*/NTP=time.google.com/' /etc/systemd/timesyncd.conf
+sudo systemctl restart systemd-timesyncd
 echo sudo apt update
 sudo apt install -y git
 cd /opt
@@ -21,7 +23,6 @@ sudo /opt/kisshome-raspi-image/install.sh
 Use [HDDRawCopy1.20Portable.exe](https://hddguru.com/software/HDD-Raw-Copy-Tool/HDDRawCopy1.20Portable.exe) or [win32diskimager-1.0.0-install.exe](https://sourceforge.net/projects/win32diskimager/files/latest/download) to make an image.
 
 ![step2](step3.png)
-
 
 ## After creation
 The ssh login is `iob` and the password is `2024=smart!`. Change the password immediately after the first reboot!
--- install.sh
+++ install.sh
@@ -32,9 +32,33 @@ curl -sLf https://iobroker.net/install.sh | bash -
 cd /opt/iobroker
 sudo iob stop
 
+# Add to /etc/sudoers.d/iobroker the "iobroker ALL=(ALL) NOPASSWD: /usr/sbin/iwlist"
+# Define the line to check/add
+LINE="iobroker ALL=(ALL) NOPASSWD: /usr/sbin/iwlist"
+FILE="/etc/sudoers.d/iobroker"
+# Check if the line is already in the file
+if ! grep -Fxq "$LINE" "$FILE"; then
+    # If not, add the line to the file
+    echo "$LINE" | sudo tee -a "$FILE"
+    echo "iwlist added to $FILE"
+else
+    echo "iwlist already exists in $FILE"
+fi
+LINE="iobroker ALL=(ALL) NOPASSWD: /usr/bin/nmcli"
+FILE="/etc/sudoers.d/iobroker"
+# Check if the line is already in the file
+if ! grep -Fxq "$LINE" "$FILE"; then
+    # If not, add the line to the file
+    echo "$LINE" | sudo tee -a "$FILE"
+    echo "nmcli added to $FILE"
+else
+    echo "nmcli already exists in $FILE"
+fi
+
 # Settings for kisshome
 sudo iob add kisshome-research
 sudo iob add welcome
+#sudo iob add network-settings
 # End of settings for kisshome
 sudo iob unsetup -y
 
